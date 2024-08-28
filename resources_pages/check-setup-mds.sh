53255a70f222a34c73a3c5eb6c50b2a1fa699c6c
Joel Ostblom <joelostblom@users.noreply.github.com>
2024-08-28 15:49:36 -0700

    correct nbconvert name

--- resources_pages/check-setup-mds.sh
+++ resources_pages/check-setup-mds.sh
@@ -172,7 +172,7 @@ if ! [ -x "$(command -v conda)" ]; then  # Check that conda exists as an executa
     echo "In order to do this after the installation process," >> check-setup-mds.log
     echo "first run 'source <path to conda>/bin/activate' and then run 'conda init'." >> check-setup-mds.log
 else
-    py_pkgs=(otter-grader=5 pandas=2 nbconvert-core=7 playwright=1 jupyterlab=4 jupyterlab-git=0 jupyterlab-spellchecker=0)
+    py_pkgs=(otter-grader=5 pandas=2 nbconvert=7 playwright=1 jupyterlab=4 jupyterlab-git=0 jupyterlab-spellchecker=0)
     # installed_py_pkgs=$(pip freeze)
     installed_py_pkgs=$(conda list | tail -n +4 | tr -s " " "=" | cut -d "=" -f -2)
     for py_pkg in ${py_pkgs[@]}; do
